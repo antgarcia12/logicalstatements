5bf257e303076723b13b2d5ebf95f3c9d3cbe229
antgarcia12 <antgarcia014@gmail.com>
2021-02-23 17:07:29 -0700

    added files and if statements

---
+++ index.js
@@ -0,0 +1,26 @@
+// let hour = 19;
+// let isWeekend = true;
+// if  (hour < 7 || hour > 23 || isWeekend) {
+//     document.write("Im sleep")
+// }
+
+// let rain = true;
+// let snow = false;
+// let sunny = true;
+
+// if (rain === true || snow === false && sunny === true) {
+//     document.write("It's okay to go outside"); 
+// }
+
+
+// let money = "zero";
+// let isBroke = "YES";
+// let nameSpace = "Test";
+// document.write( money || isBroke || nameSpace || "What??" );
+// Output = "What??"; 
+
+
+// let x = 1;
+// x === 1 || document.write("is true");
+// false || document.write("not true");
+
